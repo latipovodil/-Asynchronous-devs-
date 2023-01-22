3882f7b22c20b3a7963e271ec5765205efac7121
Odilbek <113532560+odilbekl@users.noreply.github.com>
2023-01-22 20:34:46 +0500

    my 3 sections end

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(".popular__slide").slick({
+  autoplay: true,
+  dots: true,
+  speed: 600,
+  slidesToShow: 3.96,
+});
+
+
+$(".popular__slide2").slick({
+  autoplay: true,
+  dots: true,
+  speed: 600,
+  slidesToShow: 2.5,
+});
+
+$(".popular__slide3").slick({
+  autoplay: true,
+  dots: true,
+  speed: 600,
+  slidesToShow: 1.2,
+});
+
+
+$(".our__slide").slick({
+  autoplay: true,
+  dots: true,
+  speed: 600,
+  slidesToShow: 3.03,
+});
+
+
+$(".our__slide2").slick({
+  autoplay: true,
+  dots: true,
+  speed: 600,
+  slidesToShow: 2.03,
+});
+
+$(".our__slide3").slick({
+  autoplay: true,
+  dots: true,
+  speed: 600,
+  slidesToShow: 0.8,
+});
+
+
+
