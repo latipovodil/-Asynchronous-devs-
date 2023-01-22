be391454529c9d9d642ee490d45dea04a23e5614
Odilbek <113532560+odilbekl@users.noreply.github.com>
2023-01-22 21:07:49 +0500

    end

--- js/main.js
+++ js/main.js
@@ -45,3 +45,23 @@ $(".our__slide3").slick({
 
 
 
+const userModal = document.querySelector(".user-modal")
+const registration = document.querySelector(".registration");
+const cancel = document.querySelector(".registration__cancel");
+const headerHamburger = document.querySelector(".site-header__tablet-btn");
+const tabletNav = document.querySelector(".tablet-header");
+const tabletCancel = document.querySelector(".tablet-header__cancel");
+
+userModal.addEventListener("click", () => {
+    registration.style.top = "0";
+})
+cancel.addEventListener("click", () => {
+  registration.style.top = "-150%";
+})
+
+headerHamburger.addEventListener("click", () => {
+  tabletNav.style.left = "0";
+})
+tabletCancel.addEventListener("click", () => {
+  tabletNav.style.left = "-100%";
+})
